cee60c1c92865b62e41fe0bf88382b65faf28815
lucasgomesoficial <lucasgomesbr@gmail.com>
2021-08-27 17:26:50 -0300

    Exibir estudantes da turma

---
+++ src/endpoint/studentClass.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { connection } from "../data/connection";
+
+export const studentClass = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = Number(req.params.id)
+    const result = await connection.raw(`
+      SELECT class_id,student.name,class.name from class
+      JOIN student
+      ON class.id = student.class_id
+      WHERE class.id="${id}"
+    `)
+    console.log(result[0])
+    res.status(200).send(result[0])
+  }
+  catch (error) {
+    if (typeof error === "string") {
+      res.send(error)
+    }
+    else {
+      res.status(500).send({ message: error.sqlMessage || error.message })
+    }
+  }
+}
